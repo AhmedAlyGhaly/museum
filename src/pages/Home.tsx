2f1a95cd7a7e615d79a98b15e8a3ce8a6880337a
AhmedAlyGhaly <64282056+AhmedAlyGhaly@users.noreply.github.com>
2024-12-14 20:55:42 +0200

    change image path

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ type HomeProps = {};
 export const Home: React.FC<HomeProps> = () => {
   return (
       <img
-              src="/src/assets/Ancient Egypt museum of science and innovation.jpg"
+              src="/public/ancient_egypt_museum_of_science_and_innovation.jpg"
               alt="Museum Logo"
               style={{ width: '100%', height: '100%'}}
             />
